074f2032b2add968c14c2d9037e0c27a0a573755
Nawaf <mhdnawaf2003@gmail.com>
2026-07-22 08:33:37 +0530

    Update footer to use image logo instead of text
    Replaced text logo with an image logo in the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,8 +28,8 @@ export default function Footer({ dark, showBackToTop }: FooterProps) {
           <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
             {/* Logo */}
             <a href="#home" className="font-mono font-semibold text-base">
-              
-              <span className={dark ? 'text-white' : 'text-gray-900'}>NAWAF</span>
+              <img src={logo} alt="Logo" className="h-8 w-auto" />
+              <span className={dark ? 'text-white' : 'text-gray-900'}></span>
              
             </a>
 
